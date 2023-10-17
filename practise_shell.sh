da6540a9b8b26a67a5cf59e7a0c7010b1c9ce2fb
PS8966 <132941962+PS8966@users.noreply.github.com>
2023-10-17 15:55:36 +0530

    loop code

--- practise_shell.sh
+++ practise_shell.sh
@@ -9,3 +9,30 @@ echo $?  #check for last command success
 
 my_array=(1 2 3 4 "pushpendra") #declaring an array
 echo ${my_array[@]}  #print all items in an array
+echo -n "Enter a number: "
+read num
+ 
+# store the original number
+original_num=$num
+ 
+# reverse the number
+rev=0
+while [ $num -gt 0 ]
+do
+    # get the remainder of the number
+    remainder=$(($num % 10))
+    
+    # multiply reverse by 10 then add the remainder
+    rev=$((($rev * 10) + $remainder))
+    
+    # divide the number by 10
+    num=$(($num / 10))
+done
+ 
+# check if the number is a palindrome
+if [ $original_num -eq $rev ];
+then
+    echo "$original_num is a palindrome number."
+else
+    echo "$original_num is not a palindrome number."
+fi
